1b9849732e8224aa0786f9804ed26a23b33a8b0b
Ngoc Hai <tr.ngochai23@gmail.com>
2019-11-26 20:51:09 +0700

    update code

--- app/features/pg_home.js
+++ app/features/pg_home.js
@@ -6,27 +6,34 @@ const modelCategories = require('../models/categories');
 request('https://giaoduc.net.vn/', (error, response, html) => {
     if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
-        const abc = $('.menu-heading selected').text();
-        console.log(':::::::::::',abc)
-        // $('.menu-heading selected').each((index, element) => {
+        let array1 = [];
+        let array2 = [];
+        // $('.menu-heading').each((index, element) => {
             
-            // const title = $(element)
-            //     .find('.news-list__headline-link')
-            //     .text()
-            //     .replace(/\s\s+/g, '');
+        //     const title = $(element)
+        //         .text()
+        //         .replace(/\n\s+/g, '');
 
-            // const time = $(element)
-            //     .find('.label__timestamp')
-            //     .text();
+        //     if ( title.length > 0 ) {
+        //         const href = $(element)
+        //             .attr('href');
 
-            // const data = $(element)
-            //     .find('.news-list__snippet')
-            //     .text();
+        //         array1.push({ title, href });
+        //     }
 
-            // const date_time = date_time_format(time);
-
-            // // mongo_MU_news.create({ date_time, title, data });
-
+        //     // const a = $(element)
+        //     //     .find('a')
+        //     //     .length;
+        //     // console.log
         // });
+
+
+        $('a').each((index, element) => {
+            const title = $(element)
+                .find($('.menu-heading'))
+                .text();
+            console.log(title)
+        });
+
     }
 });
