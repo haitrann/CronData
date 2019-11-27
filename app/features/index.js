b2e79cec903a1935c66e440c6a748262696ae139
Ngoc Hai <tr.ngochai23@gmail.com>
2019-11-27 14:38:57 +0700

    update code

--- app/features/index.js
+++ app/features/index.js
@@ -1,7 +1,6 @@
 // const pageHome = require('./pg_home');
 const pageListNews = require('./pg_listContent');
 
-pageListNews('https://giaoduc.net.vn/tin-hiep-hoi/451.gd');
-
 
 // pageHome('https://giaoduc.net.vn');
+pageListNews('https://giaoduc.net.vn/tin-hiep-hoi/451.gd');
